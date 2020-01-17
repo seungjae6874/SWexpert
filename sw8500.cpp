1ddc31bc338065b0c0610166a11a202b0bd305c9
seungjae6874 <qkrtmdwo1@gmail.com>
2020-01-17 23:32:55 +0900

    2020-01-17 sw8500 ver 1.1

--- sw8500.cpp
+++ sw8500.cpp
@@ -19,21 +19,26 @@ int main()
 			A[j] = k;
 		}
 		sort(A,A+N);
+		for(int i = 0; i < N; i++)
+		{
+			cout<<A[i]<<endl;
+		}
 		//가장 큰값 더하고 1 더 하고 가장 큰값 더하고 1 더하고 그다음 큰 값 더하고 
 		//마지막 값 제외 나머지값*2 + N 하면 최솟값 
 		for(int a = N-1; a >= 0; a--)
 		{
-			if(a != A[N-1])
+			if(a == N-1)
 			{
 				sum += 2*A[a];	
 			}
-			else if( a == A[N-1])
+			else if( a != N-1)
 			{
 				sum += A[a];
 			}
-			sum += N;
 			
+			cout<<"sum : "<<sum<<endl;
 		}
+		sum += N;
 		cout<<"#"<<i+1<<" "<<sum<<endl;
 		sum = 0;
 	}
