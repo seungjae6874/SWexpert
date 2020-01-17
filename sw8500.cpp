c6914120c2750f33a3353db12f24283680988f1c
seungjae6874 <qkrtmdwo1@gmail.com>
2020-01-18 00:10:26 +0900

    2020-01-18 SW 8500 final

--- sw8500.cpp
+++ sw8500.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <string.h>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 int main()
 {
-	int T;
+	int T=0;
 	cin>>T;
 	for(int i =0; i<T; i++)
 	{
+		int count = 0;
 		int sum =0;
 		int N;
-		int *A = new int[N];
+		int A[100000] = {0,};
 		cin>>N;
 		for(int j=0; j<N; j++)
 		{
@@ -19,28 +20,19 @@ int main()
 			A[j] = k;
 		}
 		sort(A,A+N);
-		for(int i = 0; i < N; i++)
-		{
-			cout<<A[i]<<endl;
-		}
-		//가장 큰값 더하고 1 더 하고 가장 큰값 더하고 1 더하고 그다음 큰 값 더하고 
-		//마지막 값 제외 나머지값*2 + N 하면 최솟값 
 		for(int a = N-1; a >= 0; a--)
 		{
 			if(a == N-1)
 			{
 				sum += 2*A[a];	
 			}
-			else if( a != N-1)
+			else
 			{
 				sum += A[a];
 			}
-			
-			cout<<"sum : "<<sum<<endl;
 		}
 		sum += N;
-		cout<<"#"<<i+1<<" "<<sum<<endl;
-		sum = 0;
+		cout<<"#"<<i+1<<" "<<sum<<endl;	
 	}
 	return 0;
 }
