40405e857f31e995cb937f5c9aa8571f171fb2cb
seungjae6874 <qkrtmdwo1@gmail.com>
2020-01-24 21:16:04 +0900

    2020-01-24 sw8821 ver 1.1

--- sw8821.cpp
+++ sw8821.cpp
@@ -10,21 +10,39 @@ int main(){
 		cin>>num;
 		char check2[] = {'0','1','2','3','4','5','6','7','8','9'}; 
 		int check[10] = {0,};//문자열에서 각 수가 몇번 나왔는지 담는 배열
-		 
-		for(int i = 0; i<10; i++){
-			for(int a = 0; num[a] != '\0'; a++){
-				if(num[a] == check2[i]){
-					check[i]++;
+		
+		for(int a = 0;  num[a] != '\0'; a++){
+			len++;
+		}
+		//cout<<len<<endl;;
+		char num2[len];
+		for(int l = 0; l<len; l++){
+			num2[l] = '\0';
+		}
+		int flag =0;
+		int index = 0;
+		for(int i = 0; i < len; i++){
+			for(int j = 0; j<len;j++){
+				if(num2[j] == num[i]){
+					num2[j] = '\0';		
+				}
+				else{
+					index++;
 				}
 			}
-		}
-		int cnt = 0;
-		for(int a =0; a<10; a++){
-			if(check[a] %2 == 1){
-				cnt++;
+			if(index == len){
+				num2[flag++] = num[i];
+				index = 0;
 			}
-			
+					
 		}
-		cout<<"#"<<i+1<<" "<<cnt<<endl;
+		int final = 0;
+		for(int f = 0; f<len; f++){
+			if(num2[f] != '\0'){
+			//	cout<<num2[f]<<endl;
+				final++;	
+			}
+		}
+		cout<<"#"<<i+1<<" "<<final<<endl;
 	}
 }
