fe178dcfd9a9d21dde2ec3d44fd735da634d35c9
seungjae6874 <qkrtmdwo1@gmail.com>
2020-02-04 18:02:19 +0900

    2020-02-04 sw8658 ver 1.0

---
+++ sw8658.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int fun(int x){
+	int len = 0;
+	int sum =0;
+	while(x !=0){
+		sum += x % 10; 
+		x /= 10;
+		len++;
+	}
+	return sum;
+	
+}
+int main(){
+	int T;
+	cin>>T;
+	for(int i = 0; i< T; i++){
+		int *num = new int[10];
+		int *sum = new int[10];
+		for(int i = 0; i < 10; i++){
+			cin>>num[i];
+			sum[i] = fun(num[i]);	
+		}
+		int min = *min_element(sum,sum+10);
+		int max = *max_element(sum,sum+10);
+		
+		cout<<"#"<<i+1<<" "<<min<<" "<<max<<endl;
+	}
+}
--- sw8821.cpp
+++ sw8821.cpp
@@ -1,48 +1,31 @@
 #include <iostream> 
-#include <string.h> 
+#include <string> 
 using namespace std;
 int main(){
 	int T=0;
 	cin>>T;
 	for(int i = 0; i < T; i++){
 		int len=0;
-		char num[10000];
+		string num;
 		cin>>num;
-		char check2[] = {'0','1','2','3','4','5','6','7','8','9'}; 
-		int check[10] = {0,};//문자열에서 각 수가 몇번 나왔는지 담는 배열
-		
-		for(int a = 0;  num[a] != '\0'; a++){
-			len++;
-		}
-		//cout<<len<<endl;;
-		char num2[len];
-		for(int l = 0; l<len; l++){
-			num2[l] = '\0';
-		}
-		int flag =0;
-		int index = 0;
-		for(int i = 0; i < len; i++){
-			for(int j = 0; j<len;j++){
-				if(num2[j] == num[i]){
-					num2[j] = '\0';		
-				}
-				else{
-					index++;
-				}
+		string num2;
+		len = num.length();
+		int len2 = 0; 
+		int a = 0;
+		int b = 0;
+		int flag = 0;
+		for(a = 0; a < len; a++){
+			num2 += num[a];
+			if((a > 0 ) &&(num2.length() != 0)){
+				for(b =0; b < num2.length()-1; b++){
+					if(num2[b] == num[a]){
+						num2.erase(b,1);
+					}
+				}	
 			}
-			if(index == len){
-				num2[flag++] = num[i];
-				index = 0;
-			}
-					
+			
 		}
-		int final = 0;
-		for(int f = 0; f<len; f++){
-			if(num2[f] != '\0'){
-			//	cout<<num2[f]<<endl;
-				final++;	
-			}
-		}
-		cout<<"#"<<i+1<<" "<<final<<endl;
+		cout<<num2.length()<<endl;
+		//cout<<"#"<<i+1<<" "<<fi<<endl;
 	}
 }
