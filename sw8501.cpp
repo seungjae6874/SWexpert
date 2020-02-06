14cea01333817246015700d08c3338151afc03ba
seungjae6874 <qkrtmdwo1@gmail.com>
2020-02-06 20:11:20 +0900

    2020-02-06 sw8501 ver 1.0

---
+++ sw8501.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+	int T;
+	cin>>T;
+	for(int i =0; i< T; i++){
+		int n;
+		cin>>n;
+		int *coin = new int[n];
+		int count = 0;
+			
+		for(int a = 0; a< n; a++){
+			coin[a] = a+1;
+		}
+		do{
+			int *coin2 = new int[n];
+			int *fb = new int[n]; //앞 뒤를 저장하는 배열 
+			for(int b = 0; b<n; b++){
+				coin2[b] = coin[b];
+				fb[b] = 0;
+				
+			}
+			for(int c = 0; c < n-1; c++){
+				for(int d = c+1; d < n; d++){
+					if(coin2[c] < coin2[d]){
+						if(fb[d] == 1){
+							fb[d] = 0;
+						}
+						else if(fb[d] == 0)
+						{
+							fb[d] = 1;
+						}
+					}
+				}
+			}
+			for(int k = 0; k < n; k++){
+				//cout<<fb[k];
+				if(fb[k] == 1){
+					count++;
+				}	
+			}
+			//cout<<endl;
+			
+		}while(next_permutation(coin, coin+n));
+		cout<<count<<endl;
+	}
+	return 0;
+}
